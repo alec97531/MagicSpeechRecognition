545d4fa57d177516157fb8a2d046a0e0eaa92e26
Alec <adelsesto@jisekihealth.com>
2023-06-16 18:40:46 -0700

    refactor code out of main into external files

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import json
 PLATFORM = "ANDROID"
 if PLATFORM == "ANDROID":
     import android
-    from android.storage import app_storage_path
-    from android.permissions import request_permissions, Permission
-from pydub import AudioSegment
-from pydub.playback import play
 from gtts import gTTS
 
 from kivy.app import App
@@ -20,8 +16,13 @@ from kivy.logger import Logger
 import os
 from prepare_card_names import prepare_card_names
 import speech_recognition as sr
-from speech_recognition.exceptions import TranscriptionNotReady
 import platform
+from app_io import play_audio, get_storage_path, init_platform, listen_in_background
+from integrations import await_assemblyai_recognition
+def read_key(secrets_fpath):
+    with open(secrets_fpath, "r") as fi:
+        key = fi.read()
+    return key
 
 Builder.load_string('''
 <MainLayout>:
@@ -38,62 +39,15 @@ Builder.load_string('''
             on_release: app.reload_cards()
         ToggleButton:
             id: mute_button
-            text: 'Microphone Listening'
+            text: 'Microphone Muted'
             on_state: app.toggle_microphone(self.state)
             size_hint_x: 0.4
-            background_color: 0, 1, 0, 1
+            background_color: 1, 0, 0, 1
     TextInput:
         id: log_label
         readonly: True
 ''')
-class MusicPlayerAndroid(object):
-    def __init__(self):
 
-        from jnius import autoclass
-        MediaPlayer = autoclass('android.media.MediaPlayer')
-        self.mplayer = MediaPlayer()
-
-        self.secs = 0
-        self.actualsong = ''
-        self.length = 0
-        self.isplaying = False
-
-    def __del__(self):
-        # self.stop()
-        # self.mplayer.release()
-        Logger.info('mplayer: deleted')
-
-    def load(self, filename):
-        try:
-            self.actualsong = filename
-            self.secs = 0
-            self.mplayer.setDataSource(filename)        
-            self.mplayer.prepare()
-            self.length = self.mplayer.getDuration() / 1000
-            Logger.info('mplayer load: %s' %filename)
-            Logger.info ('type: %s' %type(filename) )
-            return True
-        except:
-            Logger.info('error in title: %s' % filename) 
-            return False
-
-    def unload(self):
-            self.mplayer.reset()
-
-    def play(self):
-        self.mplayer.start()
-        self.isplaying = True
-        Logger.info('mplayer: play')
-
-    def stop(self):
-        self.mplayer.stop()
-        self.secs=0
-        self.isplaying = False
-        Logger.info('mplayer: stop')
-
-    def seek(self,timepos_secs):
-        self.mplayer.seekTo(timepos_secs * 1000)
-        Logger.info ('mplayer: seek %s' %int(timepos_secs))
 
 class MainLayout(BoxLayout):
     pass
@@ -101,13 +55,13 @@ class MainLayout(BoxLayout):
 
 class SpeechRecognitionApp(App):
     def build(self):
-        self.microphone_muted = False
-        if PLATFORM == "ANDROID":
-            self.storage_path = app_storage_path()+"/"
-        else:
-            self.storage_path = ""
+        self.platform = init_platform()
+        self.microphone_muted = True
+        self.storage_path = get_storage_path()
+        self.assemblyai_key = read_key(self.storage_path+"app/secrets/assemblyai_api_key.txt")
+        self.listen_stopper = None
         print(f"storage path: {self.storage_path}")
-        prepare_card_names(path=self.storage_path)
+        # prepare_card_names(path=self.storage_path)
         try:
             self.cardnames = self.load_cardnames()
         except FileNotFoundError:
@@ -116,7 +70,8 @@ class SpeechRecognitionApp(App):
             self.cardnames = self.load_cardnames()
 
         self.recognizer = sr.Recognizer()
-        # self.start_listening()
+        if self.platform == "DESKTOP":
+            self.microphone = sr.Microphone()
 
         layout = MainLayout()
         self.log_label = layout.ids.log_label
@@ -135,6 +90,7 @@ class SpeechRecognitionApp(App):
         return False
 
     def berate_match(self, card_name):
+        self.schedule_stop_listening()
         fpath = self.storage_path+"temp/"
         if not os.path.exists(fpath):
             os.mkdir(fpath)
@@ -143,45 +99,28 @@ class SpeechRecognitionApp(App):
         soundbite_path =f"{fpath}temp.mp3" 
         soundbite.save(soundbite_path)
         time.sleep(0.1)
-        if PLATFORM == "ANDROID":
-            mplayer = MusicPlayerAndroid()
-            if mplayer.load(soundbite_path):
-                mplayer.play()
-            else:
-                Logger.info(f"can't load {soundbite_path}")
+        play_audio(soundbite_path)
+
+
+    
+    def handle_recognition_desktop(self, recognizer, audio):
+        # replace this from master's version
+        pass
+
+    def toggle_microphone(self, state):
+        mute_button = self.root.ids.mute_button
+        if self.microphone_muted:
+            self.microphone_muted = False
+            mute_button.text = "Microphone Listening"
+            mute_button.background_color = (0, 1, 0, 1)  # Red color
+            self.start_listening()
         else:
-            voiceover = AudioSegment.from_mp3(soundbite_path)
-            play(voiceover)
+            self.stop_listening()
 
-    def handle_recognition(self, recognizer, audio):
-        # test snippet
-        if not audio:
-            if PLATFORM == "ANDROID":
-                audio_path = f"{self.storage_path}app/test_input.wav"
-            else:
-                audio_path = f"{self.storage_path}test_input.wav"
-
-            # audio_file = sr.AudioFile(audio_path)
-            # with audio_file as source:
-            #     audio = recognizer.record(source)
-            # # text = recognizer.recognize_google(audio)
-            # # test snippet
+    def handle_audio_chunk(self, filename):
         if self.microphone_muted:
             return
-        retries = 0
-        job_name = None
-        _audio_path = audio_path
-        while(True):
-            try:
-                text, confidence = recognizer.recognize_assemblyai(_audio_path, "", job_name=job_name)
-            except TranscriptionNotReady as e:
-                time.sleep(0.5)
-                _audio_path = None
-                if not job_name:
-                    job_name = e.job_name
-            else:
-                break
-
+        text = await_assemblyai_recognition(filename, self.recognizer, self.assemblyai_key)
         try:
             self.schedule_log("I: " + text)
             card_match = self.check_for_cardname(text.lower())
@@ -191,37 +130,29 @@ class SpeechRecognitionApp(App):
         except sr.UnknownValueError:
             self.schedule_log("Speech Recognition could not understand audio")
         except sr.RequestError as e:
-            self.schedule_log("Could not request results from Google Speech Recognition service; {0}".format(e))
-        # except Exception as e:
-        #     self.schedule_log(f"Hit: {e}")
-
-    def toggle_microphone(self, state):
-        # test snippet
-        self.handle_recognition(self.recognizer, None)
-        return
-        mute_button = self.root.ids.mute_button
-        if state == 'down':
-            self.microphone_muted = True
-            mute_button.text = "Microphone Muted"
-            mute_button.background_color = (1, 0, 0, 1)  # Red color
-        else:
-            self.microphone_muted = False
-            mute_button.text = "Microphone Listening"
-            mute_button.background_color = (0, 1, 0, 1)  # Red color
-
-    # def start_listening(self):
-    #     # native desktop implementation
-    #     with self.microphone as source:
-    #         self.recognizer.adjust_for_ambient_noise(source)
-    #     self.recognizer_callback = self.recognizer.listen_in_background(self.microphone, self.handle_recognition)
+            self.schedule_log("Could not request results from Speech Recognition service; {0}".format(e))
+ 
     def start_listening(self):
-        # ideally, this listens to the mic in the background and calls a callback for "handle_recognition"
-        pass
-    # def start_listening(self):
-    #     # nonsense chatgpt hallucination bullshit?
-    #     (id, result, error) = self.droid.recognizeSpeech("say something")
-    #     print(f"{result} : {error}")
-    
+        if self.platform == "ANDROID":
+            self.listen_stopper = listen_in_background(self.handle_audio_chunk)
+            self.schedule_log("began listening")
+        else:
+            with self.microphone as source:
+                self.recognizer.adjust_for_ambient_noise(source)
+            self.recognizer_callback = self.recognizer.listen_in_background(self.microphone, self.handle_recognition)
+
+    def schedule_stop_listening(self):
+        Clock.schedule_once(lambda dt: self.stop_listening())
+    def stop_listening(self):
+        mute_button = self.root.ids.mute_button
+        self.microphone_muted = True
+        mute_button.text = "Microphone Muted"
+        mute_button.background_color = (1, 0, 0, 1)  # Red color
+        if self.listen_stopper is not None:
+            self.listen_stopper()
+            self.schedule_log("stopped listening")
+            self.listen_stopper = None
+
     def on_stop(self):
         self.recognizer_callback(wait_for_stop=False)
 
@@ -247,8 +178,6 @@ class SpeechRecognitionApp(App):
         prepare_card_names(path=self.storage_path, format_filter=format_filter)
         self.cardnames = self.load_cardnames()
 if __name__ == '__main__':
-    if PLATFORM == "ANDROID":
-        request_permissions([Permission.INTERNET, Permission.RECORD_AUDIO])
     print(f"system : {platform.system()}")
     print(f"machine : {platform.machine()}")
 
